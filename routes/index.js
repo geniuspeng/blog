6a64cf79c398f0ccf85c37e3e9361d05d8aa2d52
geniuspeng <geniuspeng@qq.com>
2015-04-09 08:29:59 +0800

    增加目录浏览功能

--- routes/index.js
+++ routes/index.js
@@ -162,6 +162,22 @@ router.post('/upload',function(req,res){
     }
 });
 
+router.get('/archive', function(req, res){
+    Post.getArchive(function (err,posts){
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        res.render('archive', {
+            title: '存档',
+            posts: posts,
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+});
+
 router.get('/u/:name', function(req, res){
     //检查用户是否存在
     User.get(req.params.name, function (err, user) {
